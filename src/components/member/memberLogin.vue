bae06dbbedf0407a8783c96c952e7cecb19e5044
orange11th <orange1th@gmail.com>
2023-11-10 15:19:54 +0900

    별이유없는커민

--- src/components/member/memberLogin.vue
+++ src/components/member/memberLogin.vue
@@ -3,13 +3,25 @@
 <template>
   <div>
     <div>
-      <input type="text" placeholder="아이디" />
+      <input class="login-input" type="text" placeholder="아이디" />
     </div>
-    <div><input type="password" placeholder="패스워드" /></div>
+    <div><input class="login-input" type="password" placeholder="패스워드" /></div>
     <div>
-      <button>로그인</button>
+      <button class="login-button">로그인</button>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.login-input {
+  width: 350px;
+  height: 45px;
+  background-color: rgb(231, 231, 231);
+  border: 0px;
+  padding-left: 15px;
+  border-radius: 15px;
+  margin: 10px;
+}
+.login-button {
+}
+</style>
