1db3e0211b8ee23ef02d8c5e7a734eae38e05613
orange11th <orange1th@gmail.com>
2023-11-13 17:53:26 +0900

    로그인 회원가입 css

--- src/components/member/memberLogin.vue
+++ src/components/member/memberLogin.vue
@@ -3,12 +3,12 @@ import { reactive, ref } from "vue";
 import { useRouter } from "vue-router";
 import { memberLogin } from "@/api/member";
 
-const router = useRouter()
+const router = useRouter();
 
 const param = reactive({
   userId: "",
   userPass: "",
-})
+});
 
 const memberInfo = ref({});
 
@@ -25,35 +25,49 @@ function login() {
       }
     },
     (error) => {
-      console.log(error)
+      console.log(error);
     }
-  )
+  );
 }
 
 function moveMemberInfo() {
-  router.replace({name: "member-info", params: {memberInfo: JSON.stringify(memberInfo.value)}})
+  router.replace({ name: "member-info", params: { memberInfo: JSON.stringify(memberInfo.value) } });
 }
 
-function moveRegist(){
-  router.push({name: "member-regist"})
+function moveRegist() {
+  router.push({ name: "member-regist" });
 }
-
 </script>
 
 <template>
-  <h2>로그인</h2>
-  <div>
+  <div class="container text-center">
+    <h1>로그인</h1>
     <div>
-      <input class="login-input" type="text" placeholder="아이디" v-model="param.userId" />
-    </div>
-    <div><input class="login-input" type="password" placeholder="패스워드" v-model="param.userPass" /></div>
-    <div>
-      <button class="move-regist-button" @click="moveRegist">회원가입</button>
-      <button class="login-button" @click="login">로그인</button>
+      <span>아이디가 없나요? 지금 바로 </span>
+      <a href="" @click.prevent="moveRegist">회원가입</a>
+      <div>
+        <input class="login-input" type="text" placeholder="ID" v-model="param.userId" />
+      </div>
+      <div>
+        <input
+          class="login-input"
+          type="password"
+          placeholder="Password"
+          v-model="param.userPass"
+        />
+      </div>
+      <div>
+        <button class="login-button" @click="login">Login</button>
+      </div>
     </div>
   </div>
 </template>
+
 <style scoped>
+h1 {
+  margin: 50px;
+}
+
 .login-input {
   width: 350px;
   height: 45px;
@@ -61,10 +75,17 @@ function moveRegist(){
   border: 0px;
   padding-left: 15px;
   border-radius: 15px;
-  margin: 10px;
+  margin: 15px;
 }
 
 .login-button {
+  color: white;
+  width: 350px;
+  height: 45px;
+  background-color: rgb(50, 62, 62);
+  border: 0px;
+  padding-left: 15px;
+  border-radius: 15px;
   margin: 10px;
 }
 </style>
--- src/components/member/memberRegist.vue
+++ src/components/member/memberRegist.vue
@@ -1,65 +1,120 @@
 <script setup>
-import { reactive, ref } from "vue";
+import { reactive, ref, watch } from "vue";
 import { useRouter } from "vue-router";
 import { memberRegist } from "@/api/member";
 
-const router = useRouter()
+const router = useRouter();
 
 const param = reactive({
-    userId: "",
-    userPass: "",
-    userName: "",
-    email: "",
-    phone: "",
-})
+  userId: "",
+  userPass: "",
+  userName: "",
+  email: "",
+  phone: "",
+});
+
+const userPassCheck = ref("");
+const diffPass = ref(false);
+
+watch([param, userPassCheck], ([nV1, nV2], [oV1, oV2]) => {
+  diffPass.value = nV1.userPass !== nV2;
+});
 
 function regist() {
-    memberRegist(param, (response) => { alert('회원가입 성공');console.log('성공');moveLogin(); }, (error) => { alert('회원가입 실패!');console.error() })
+  memberRegist(
+    param,
+    (response) => {
+      alert("회원가입 성공");
+      console.log("성공");
+      moveLogin();
+    },
+    (error) => {
+      alert("회원가입 실패!");
+      console.error();
+    }
+  );
 }
 
-function moveLogin(){
-    router.push({name:"member-login"})
+function moveLogin() {
+  router.push({ name: "member-login" });
 }
-
 </script>
 
 <template>
+  <div class="container text-center">
     <div>
-        <h2>회원가입 페이지</h2>
+      <h1>회원가입</h1>
     </div>
     <div>
-        <div>
-            <input class="regist-input" type="text" placeholder="아이디" v-model="param.userId" />
-        </div>
-        <div>
-            <input class="regist-input" type="password" placeholder="패스워드" v-model="param.userPass" />
-        </div>
-        <div>
-            <input class="regist-input" type="text" placeholder="닉네임" v-model="param.userName" />
-        </div>
-        <div>
-            <input class="regist-input" type="text" placeholder="이메일" v-model="param.email" />
-        </div>
-        <div>
-            <input class="regist-input" type="pastextsword" placeholder="전화번호" v-model="param.phone" />
-        </div>
-        <div>
-            <button class="regist-button" @click="regist">회원가입</button>
-        </div>
+      <div>
+        <input class="regist-input" type="text" placeholder="아이디" v-model="param.userId" />
+      </div>
+      <div>
+        <input
+          class="regist-input"
+          type="password"
+          placeholder="비밀번호"
+          v-model="param.userPass"
+        />
+      </div>
+      <div>
+        <input
+          class="regist-input"
+          type="password"
+          placeholder="비밀번호확인"
+          v-model="userPassCheck"
+          style="margin: 15px 15px 0px 15px"
+        />
+      </div>
+      <p class="diff-pass" :style="[diffPass ? '' : 'visibility:hidden']">비밀번호가 다릅니다</p>
+      <div>
+        <input
+          class="regist-input"
+          type="text"
+          placeholder="닉네임"
+          v-model="param.userName"
+          style="margin: 13.5px 15px 15px 15px"
+        />
+      </div>
+      <div>
+        <input class="regist-input" type="text" placeholder="이메일" v-model="param.email" />
+      </div>
+      <div>
+        <input class="regist-input" type="text" placeholder="전화번호" v-model="param.phone" />
+      </div>
+      <div>
+        <button class="regist-button" @click="regist">Register</button>
+      </div>
     </div>
+  </div>
 </template>
 <style scoped>
+h1 {
+  margin: 50px;
+}
+.diff-pass {
+  font-size: 11px;
+  color: red;
+  margin: 0px;
+}
 .regist-input {
-    width: 350px;
-    height: 45px;
-    background-color: rgb(231, 231, 231);
-    border: 0px;
-    padding-left: 15px;
-    border-radius: 15px;
-    margin: 10px;
+  width: 350px;
+  height: 45px;
+  background-color: rgb(231, 231, 231);
+  border: 0px;
+  padding-left: 15px;
+  border-radius: 15px;
+  margin: 15px;
 }
 
 .regist-button {
-    margin: 10px;
+  color: white;
+  width: 350px;
+  height: 45px;
+  background-color: rgb(50, 62, 62);
+  border: 0px;
+  padding-left: 15px;
+  border-radius: 15px;
+  margin: 10px;
 }
 </style>
