a9da14f10ed20ce884fd9fff1d14c72eea8ba503
blog704 <blog704@naver.com>
2023-11-17 21:01:17 +0900

    Feat: plan

--- src/components/team/PlanList.vue
+++ src/components/team/PlanList.vue
@@ -2,25 +2,29 @@
   <div class="playground">
     <div v-for="(item, idx) in state.lists" :key="item.id" class="col"
          @drop.prevent="onDrop($event, idx)"
-         @dragenter.prevent
-         @dragover.prevent
-    >
-      <div v-for="(numItem, numIdx) in item.numberList" :key="numIdx"
-           class="box"
-           @dragstart="startDrag($event, numItem)"
+         @dragover.prevent>
+      <div v-for="(numItem, numIdx) in item.numberList" :key="numIdx" class="box"
            draggable="true"
-      >
+           @dragstart="startDrag($event, numItem, idx)">
         <p>{{ numItem.content }}</p>
+        <p>{{ numItem.date }}</p>
       </div>
-      <!-- 드래그 오버 이벤트를 추가한 추가 버튼 -->
-      <button @click="addItem(idx)" @dragover.prevent>+</button>
+      <button @click="addItem(idx)">+</button>
     </div>
   </div>
+  <!-- 새로운 아이템 입력 필드 -->
+  <div class="new-item-input">
+    <input type="text" v-model="newItem.content" placeholder="내용 입력" />
+    <input type="date" v-model="newItem.date" />
+    <button @click="register">아이템 추가</button>
+  </div>
 </template>
+
 
   
 <script setup>
   import { reactive } from 'vue';
+
   
   const state = reactive({
     lists: [
@@ -69,7 +73,57 @@ const addItem = (colNum) => {
   
     state.lists[colNum].numberList.push(targetItem);
     state.lists[targetIdx].numberList.splice(state.lists[targetIdx].numberList.indexOf(targetItem), 1);
+};
+// 새 컬럼 추가 함수
+const register = () => {
+    if (newItem.content && newItem.date) {
+      const newColumn = {
+        id: state.lists.length + 1, // 다음 ID를 생성
+        numberList: [{ content: newItem.content, date: newItem.date }]
+      };
+      state.lists.push(newColumn);
+
+      // 입력 필드 초기화
+      newItem.content = '';
+      newItem.date = '';
+    }
   };
+
+const newItem = reactive({
+    content: '',
+    date: ''
+  });
+
+  const addNewItem = () => {
+    if (newItem.content && newItem.date) {
+      state.lists.forEach(list => {
+        list.numberList.push({ ...newItem });
+      });
+      newItem.content = '';
+      newItem.date = '';
+    }
+  };
+
+  const deleteItem = (colNum, itemIndex) => {
+    state.lists[colNum].numberList.splice(itemIndex, 1);
+
+    const addItem = (colNum) => {
+    state.lists[colNum].numberList.push({ ...newItem });
+    newItem.content = '';
+    newItem.date = '';
+  };
+
+  const addNewItem = () => {
+    state.lists.push({
+      id: Date.now(),
+      numberList: [{ ...newItem }]
+    });
+    newItem.content = '';
+    newItem.date = '';
+  };
+};
+
+
   </script>
   
   <style scoped>
