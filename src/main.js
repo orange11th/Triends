6206494c132b7db93620895e8b90a0b00043e8b4
xion <xion2664@gmail.com>
2023-11-20 15:18:00 +0900

    icon 추가

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import { createPinia } from "pinia";
 import { createNaverMap } from "vue3-naver-maps";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-
 import App from "./App.vue";
 import router from "./router";
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/base.css";
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
