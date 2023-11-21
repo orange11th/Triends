8f245f8789672fbe23958e840c7ef14a23358367
orange11th <orange1th@gmail.com>
2023-11-21 22:24:33 +0900

    Feat:회원정보수정

--- src/components/member/memberInfo.vue
+++ src/components/member/memberInfo.vue
@@ -8,26 +8,40 @@ const route = useRoute();
 const router = useRouter();
 const memberStore = useMemberStore();
 
-const text = ref("클릭하여 제목을 편집해주세요");
-const isEditing = ref(false);
 const clickedItem = ref(null); // 클릭된 아이템을 추적하기 위한 ref 생성
 
-const startEditing = () => {
-  isEditing.value = true;
+const startEditing = (no) => {
+  isEditing.value[no] = true;
+  changed.value=true;
 };
 
-const stopEditing = () => {
-  isEditing.value = false;
+const stopEditing = (no) => {
+  isEditing.value[no] = false;
+  changed.value=true;
 };
 
 const { userLogout, checkToken } = memberStore;
 const { isLogin, isValidToken, userInfo } = storeToRefs(memberStore);
+
+const isEditing = ref([false,false,false,false,false])
+
+const changed = ref(false);
+
+const changeUserInfo = ref({
+  userId: "",
+  userPass: "",
+  userName: "",
+  email: "",
+  phone: ""
+});
 
 onMounted(() => {
   checkToken(sessionStorage.getItem("accessToken"));
   if (!isValidToken.value) {
     alert("토큰이 만료되었습니다.");
     router.replace({ name: "member-login" });
+  }else{
+    changeUserInfo.value = {...userInfo.value};
   }
 });
 </script>
@@ -37,30 +51,44 @@ onMounted(() => {
     <div class="row">
       <!-- 사진 div (왼쪽) -->
       <div class="col-md-6 image-div">
-        <img src="@/assets/img/slide/5.jpg" class="img-fluid" alt="" />
+        <img class="profile-image img-fluid" src="@/assets/img/slide/5.jpg" alt="" />
       </div>
       <!-- 개인정보 div (오른쪽) -->
       <div class="col-md-6">
         <div class="col">
           <!-- 계획 제목 -->
           <div class="plan-title">
-            <div class="editable-text" v-if="!isEditing" @click="startEditing">
-              <div v-if="text === ''">클릭하여 제목을 편집해주세요</div>
-              <!-- 클릭하여 제목을 편집해주세요 -->
-              {{ userInfo.userId }}
-              {{ userInfo.userName }}
+            <div style="padding:8px;">
+              {{ changeUserInfo.userId }}
             </div>
-            <input v-else v-model="text" @blur="stopEditing" placeholder="제목 입력" />
+            <!-- <input v-else v-model="changeUserInfo.userId" @blur="stopEditing(0)" placeholder="변경할 아이디" /> -->
           </div>
-          <!-- 계획 제목 -->
-          <div class="plan-title">
-            <div class="editable-text" v-if="!isEditing" @click="startEditing">
-              <div v-if="text === ''">클릭하여 제목을 편집해주세요</div>
-              클릭하여 제목을 편집해주세요
-              <!-- {{ userInfo.userId }} -->
+          <div class="plan-title">  
+            <div class="editable-text" v-if="!isEditing[1]" @click="startEditing(1)">
+              {{ changeUserInfo.userName }}
             </div>
-            <input v-else v-model="text" @blur="stopEditing" placeholder="제목 입력" />
+            <input v-else v-model="changeUserInfo.userName" @blur="stopEditing(1)" placeholder="변경할 이름" />
           </div>
+          <div class="plan-title">  
+            <div class="editable-text" v-if="!isEditing[2]" @click="startEditing(2)">
+              <span v-if="!changeUserInfo.userPass">비밀번호 변경</span>
+              <span v-else>변경할 비밀번호: {{changeUserInfo.userPass}}</span> 
+            </div>
+            <input v-else v-model="changeUserInfo.userPass" @blur="stopEditing(2)" placeholder="변경할 비밀번호" />
+          </div>
+          <div class="plan-title">  
+            <div class="editable-text" v-if="!isEditing[3]" @click="startEditing(3)">
+              {{ changeUserInfo.email }}
+            </div>
+            <input v-else v-model="changeUserInfo.email" @blur="stopEditing(3)" placeholder="변경할 이메일" />
+          </div>
+          <div class="plan-title">  
+            <div class="editable-text" v-if="!isEditing[4]" @click="startEditing(4)">
+              {{ changeUserInfo.phone }}
+            </div>
+            <input v-else v-model="changeUserInfo.phone" @blur="stopEditing(4)" placeholder="변경할 전화번호" />
+          </div>
+          <button v-if="changed">수정하기</button>
         </div>
       </div>
     </div>
@@ -68,6 +96,10 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.profile-image {
+  border-radius: 100%;
+}
+
 .plan-title {
   font-size: 30px;
   width: 480px; /* 고정된 너비 */
@@ -79,6 +111,7 @@ onMounted(() => {
   align-items: center;
   justify-content: center;
   background: white;
+  margin: 20px 0;
 }
 
 .editable-text {
