b901ba498d15916f6bf3b180842b1c7f2ee00aa3
xion <xion2664@gmail.com>
2023-11-23 23:18:19 +0900

    d

--- src/components/board/BoardList.vue
+++ src/components/board/BoardList.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 import { ref, onMounted, computed } from "vue";
 import { useRouter } from "vue-router";
 import { listArticle, listArticleByLike } from "@/api/board";
@@ -17,28 +16,29 @@ const memberStore = useMemberStore();
 
 const { userInfo } = storeToRefs(memberStore);
 
-
 // computed 속성을 사용하여 userInfo가 있으면 userId를, 없으면 null을 반환
 const userId = computed(() => {
   return userInfo.value ? userInfo.value.userId : "비회원";
 });
 
 const getArticleList = () => {
-  listArticle(({ data }) => {
-    articles.value = data.articles;
-  },
+  listArticle(
+    ({ data }) => {
+      articles.value = data.articles;
+    },
     (error) => {
-    console.log(error)
+      console.log(error);
     }
   );
 };
 
 const getArticleListByLike = () => {
-  listArticleByLike(({ data }) => {
-    articles.value = data.articles;
-  },
+  listArticleByLike(
+    ({ data }) => {
+      articles.value = data.articles;
+    },
     (error) => {
-    console.log(error)
+      console.log(error);
     }
   );
 };
@@ -50,7 +50,7 @@ const moveWrite = () => {
 // 삭제 이벤트 핸들러
 const handleDeleteArticle = (articleNo) => {
   // 해당하는 글 번호를 가진 아이템을 배열에서 제거
-  articles.value = articles.value.filter(article => article.no !== articleNo);
+  articles.value = articles.value.filter((article) => article.no !== articleNo);
 };
 
 const selectedOption = ref("1"); // 기본 선택 옵션
@@ -58,8 +58,7 @@ const sortChange = (event) => {
   selectedOption.value = event.target.value;
   if (selectedOption.value == "1") {
     getArticleList();
-  }
-  else {
+  } else {
     getArticleListByLike();
   }
 };
@@ -72,39 +71,34 @@ const handleLikeClicked = () => {
 </script>
 
 <template>
- <div class="container">
+  <div class="container">
     <div class="row justify-content-center">
       <div class="col-lg-10">
-
-        
         <div class="action-buttons">
-
-        <select id="sortBox" @change="sortChange">
-          <option value="1">최신순</option>
-          <option value="2">인기순</option>
-        </select>
+          <select id="sortBox" @change="sortChange">
+            <option value="1">최신순</option>
+            <option value="2">인기순</option>
+          </select>
           <button id="save" @click="moveWrite">리뷰 작성</button>
         </div>
 
         <div class="board-list">
-                <BoardListItem
-                  v-for="article in articles"
-                  :key="article.no"
-                  :article="article"
-                  :userId="userId"
-                  @delete="handleDeleteArticle"
-                  @like-clicked="handleLikeClicked"
-                ></BoardListItem>
+          <BoardListItem
+            v-for="article in articles"
+            :key="article.no"
+            :article="article"
+            :userId="userId"
+            @delete="handleDeleteArticle"
+            @like-clicked="handleLikeClicked"
+          ></BoardListItem>
         </div>
       </div>
     </div>
   </div>
-
-  
 </template>
 
 <style scoped>
-  .board-list {
+.board-list {
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
@@ -122,7 +116,8 @@ const handleLikeClicked = () => {
 }
 
 #save:hover {
-  box-shadow: inset -5px 5px 15px -5px #b0d9b1, inset 0 -5px 15px -5px #b0d9b1, 2px 5px 5px darkgreen;
+  box-shadow: inset -5px 5px 15px -5px #b0d9b1, inset 0 -5px 15px -5px #b0d9b1,
+    2px 5px 5px darkgreen;
   cursor: pointer;
 }
 
@@ -131,26 +126,25 @@ const handleLikeClicked = () => {
   margin-left: 20px;
   width: 150px;
   border: 2px solid #b0d9b1;
-  border-radius: 25px; 
-  font-size: 16px; 
-  padding: 10px 20px; 
+  border-radius: 25px;
+  font-size: 16px;
+  padding: 10px 20px;
   cursor: pointer;
 }
 
-
-
 .action-buttons {
-    display: flex; /* Flexbox 적용 */
-    justify-content: space-between; /* 양 끝 정렬 */
-    /* margin-top: 30px; */
-    margin-bottom: 20px;
-  }
+  display: flex; /* Flexbox 적용 */
+  justify-content: space-between; /* 양 끝 정렬 */
+  /* margin-top: 30px; */
+  margin-bottom: 20px;
+}
 
-  #save, #sortBox {
-    margin-left: 20px;
-  }
+#save,
+#sortBox {
+  margin-left: 20px;
+}
 
-  #save {
-    margin-right: 15px;
-  }
+#save {
+  margin-right: 15px;
+}
 </style>
